d425833ef79e6faec7c93a005c2ab48818911019
Falnadaf <101874631+Falnadaf@users.noreply.github.com>
2024-04-21 18:08:13 +0200

    Update index.js

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'https://storehouse.onrender.com'
+export const BACKEND_URL = 'https://bin4.onrender.com'
 // export const HOST_URL = 'https://storehouse.onrender.com'
 // export const BACKEND_URL = 'http://localhost:8080'
 export const HOST_URL = 'https://gaming-console.netlify.app'
